7a9dcc8ecb6dc0c79c9ab0d65692e28ee385e16d
drobotilonna <drobotilonna@gmail.com>
2022-11-29 19:42:19 +0100

    + function getValue

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const people =[
     { id:1, name:"hifts", age: 23},
     {id: 2,name:"kola", age:45}
+
 ]
+
+// let res= []
+
+// people.forEach(function (el){
+//      res.push(el.name)
+    
+// })
+
+
+function getValue(array,key){
+  let res = []
+  array.forEach(function (el){
+         res.push(el.key)
+        
+  })
+  return res;     
+}
+
+
+console.log(getValue(people,"name"))
